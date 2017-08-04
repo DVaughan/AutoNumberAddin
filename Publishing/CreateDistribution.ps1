679f24bf64ef17c8ca2023d8fa3ed4457b70f041
Daniel Vaughan <dbvaughan@gmail.com>
2017-08-04 18:12:57 +0200

    Prepared for Addin manager.

---
+++ Publishing/CreateDistribution.ps1
@@ -0,0 +1,30 @@
+cd "$PSScriptRoot" 
+
+$src = "$env:appdata\Markdown Monster\Addins\AutoNumberAddin"
+
+"Cleaning up build files..."
+del ..\Build\addin.zip
+
+remove-item -recurse -force ..\Build\Distribution
+md ..\Build\Distribution
+
+"Copying files..."
+copy $src\*.dll ..\Build\Distribution
+copy version.json ..\Build\Distribution
+copy version.json ..\Build\
+
+[Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+
+function ZipFiles($zipfilename, $sourcedir)
+{
+   Add-Type -Assembly System.IO.Compression.FileSystem
+   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+   [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir,
+        $zipfilename, $compressionLevel, $false)
+}
+
+"Zipping up setup file..."
+$source = (get-item "$PSScriptRoot").parent.FullName + "\Build\Distribution\*.*"
+$zipPath = (get-item "$PSScriptRoot").parent.FullName + "\Build\addin.zip"
+
+Compress-Archive -Path $source -CompressionLevel Optimal -DestinationPath $zipPath
---
+++ Publishing/Example.md
@@ -0,0 +1,47 @@
+The following shows an example figure. (See Figure 1.)
+
+<figure><img src='icon_60.png'><figcaption>Figure 1. The first figure</figcaption></figure>
+
+
+Another image is below. (See Figure 2.)
+
+<figure><img src='icon_60.png'><figcaption>Figure 2. Another figure</figcaption></figure>
+
+The method iterates 10 times. (See Listing 1.)
+
+**Listing 1.** Some method1
+
+```
+for (var i = 0; i < 10; i++)
+{
+	...
+}
+```
+
+The method iterates 10 times. (See Listing 2.)
+
+**Listing 2.** Some method1
+
+```
+for (var i = 0; i < 10; i++)
+{
+	...
+}
+```
+A table. (See Table 1.)
+
+**Table 1.** A Table
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
+
+
+
+
+
+
+
